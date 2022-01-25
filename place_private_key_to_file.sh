edd3c7802c901789fc01261be5123bdf73fe3ae0
Vikentsi Lapa <Vikentsi_Lapa@epam.com>
2022-01-25 07:53:09 +0300

    add private file to config

--- place_private_key_to_file.sh
+++ place_private_key_to_file.sh
@@ -1,5 +1,6 @@
 # order of VMs start is important for first time
 vagrant up agentnode
+vagrant ssh-config agentnode > config
 # it will create private key
-awk '{print "                " $0}'  $(grep.exe IdentityFile config | grep agentnode  | cut -d ' ' -f 4) >> casc_configs/private_file.yaml
+awk '{print "                " $0}'  $(grep IdentityFile config | grep agentnode  | cut -d ' ' -f 4) >> casc_configs/private_file.yaml
 vagrant up server
